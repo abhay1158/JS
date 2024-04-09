39e336e9507b6d34709d92db8c280615c820ec3d
abhay1158 <abhayp.singh200029@gmail.com>
2024-04-09 23:19:54 +0530

    learing Till forEach loop AfterThat event, async,promises,fetch

---
+++ functionsUnderstanding.js
@@ -0,0 +1,29 @@
+function abc() {
+  return 1;
+}
+
+
+let storeRes = abc();
+console.log(storeRes);
+
+function abhay() {
+  let k = 120;
+  return function kunal() {
+    let m = 10;
+    console.log(k);
+  };
+  // console.log(k);
+}
+
+
+
+let storeRes2 = abhay();
+console.log(storeRes2); //it will gives the exact function expression
+
+
+
+// invoke Kunal function
+console.log(storeRes2()); // it will gives the result of kunal() i.e, inner function
+
+
+
